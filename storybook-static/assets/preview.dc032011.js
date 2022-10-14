d00226612a5ec3cb2397fea0d77984b9b5ca68e9
Cintia Ribeiro <cintiarbrandao@gmail.com>
2022-10-13 21:16:34 -0300

    first commit

---
+++ storybook-static/assets/preview.dc032011.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.fba6c9d8.js";import"./iframe.6523a6ae.js";import"./es.map.constructor.fc339878.js";import"./es.number.to-fixed.fef5f792.js";import"./index.52e4aad1.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.dc032011.js.map
